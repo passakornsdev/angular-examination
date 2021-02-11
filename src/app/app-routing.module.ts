724b6e3dcf477aa7e0afea6a382b24cb6d00fa1d
passakorns <passakorns@wisesoft.co.th>
2021-02-12 00:16:23 +0700

    generate home and header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './core/pages/home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent},
   {
     path: 'understanding-component',
     loadChildren: () => import('./module/understanding-component/understanding-component.module')
       .then(file => file.UnderstandingComponentModule)
-  }
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {OauthTokenInterceptor} from './interceptors/oauth-token.interceptor';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, HomeComponent],
   imports: [
     CommonModule,
     HttpClientModule
   ],
+  exports: [
+    HeaderComponent
+  ],
   providers: [
     OauthTokenInterceptor,
-    { provide: HTTP_INTERCEPTORS, useClass: OauthTokenInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: OauthTokenInterceptor, multi: true},
   ]
 })
 export class CoreModule { }
