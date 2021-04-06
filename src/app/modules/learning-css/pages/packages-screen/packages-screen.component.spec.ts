60672c4c0e0ad47ab93ef2cd5d3b3df1274c454d
Passakorn Sukhawvadeekul <passakorn.sukhawvadeekul@allianz.com>
2021-04-06 20:11:44 +0700

    add packages page

---
+++ src/app/modules/learning-css/pages/packages-screen/packages-screen.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PackagesScreenComponent } from './packages-screen.component';
+
+describe('PackagesScreenComponent', () => {
+  let component: PackagesScreenComponent;
+  let fixture: ComponentFixture<PackagesScreenComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PackagesScreenComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PackagesScreenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/learning-css/pages/packages-screen/packages-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-packages-screen',
+  templateUrl: './packages-screen.component.html',
+  styleUrls: ['./packages-screen.component.css']
+})
+export class PackagesScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
