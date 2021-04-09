cd2527eae5e51612d337e17c0828408ecfc3b0e5
Passakorn Sukhawvadeekul <passakorn.sukhawvadeekul@allianz.com>
2021-04-10 02:08:00 +0700

    create mobile nav with toggle mobile nav button

--- src/app/modules/learning-css/components/header/header.component.ts
+++ src/app/modules/learning-css/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('mobileNav') mobileNav: ElementRef | undefined;
+  @ViewChild('backDrop') backDrop: ElementRef | undefined;
+  constructor(private renderer2: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  showMobileNav() {
+    if(this.mobileNav && this.backDrop) {
+      this.renderer2.addClass(this.mobileNav.nativeElement, 'open');
+      this.renderer2.addClass(this.backDrop.nativeElement, 'open');
+    }
+  }
+
+  closeMobileNav() {
+    if(this.mobileNav && this.backDrop) {
+      this.renderer2.removeClass(this.mobileNav.nativeElement, 'open');
+      this.renderer2.removeClass(this.backDrop.nativeElement, 'open');
+    }
   }
 
 }
